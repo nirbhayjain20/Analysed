cddd7c875abb45f1e6d653842e49e659298d77dd
kunalwaje <kunalwaje28@gmail.com>
2021-08-16 12:16:38 +0530

    Completed Task Created Page

---
+++ connection1.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "analysed";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+	//echo"connection Ok" ;
+}
+else
+{
+	echo "Connection Failed";
+}
+
+
+
+?>
